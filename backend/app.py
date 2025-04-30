46a534dbaab7416cf06dba396dc1acd8320bc00f
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-04-30 16:36:10 +0600

    whole chatbot is ready

--- backend/app.py
+++ backend/app.py
@@ -32,4 +32,74 @@ class UserInput(BaseModel) :
     role : str = 'user'
     conversation_id : str 
 
+class Conversation : 
+    def __init__(self) :
+        self.messages : List[Dict[str, str]] = [
+            {"role" : 'system' , "content": "You are a useful AI assistant."}
+        ]
+        self.active : bool = True 
 
+# In memory dictionary to hold active conversations during app's runtime 
+conversations : Dict[str, Conversation] = {}
+
+def query_groq_api(conversation : Conversation) -> str : 
+    try: 
+        completion = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=conversation.messages,
+            temperature=1,
+            max_tokens=1024,
+            top_p=1,
+            stream=True,
+            stop=None,
+        )
+
+        response = ""
+        for chunk in completion : 
+            response += chunk.choices[0].delta.content or ""
+
+        return response 
+    
+    except Exception as e :
+        raise HTTPException(status_code=500, detail=f"Error with Groq API : {str(e)}")
+
+
+
+def get_or_create_conversation(conversation_id : str) -> Conversation : 
+    if conversation_id not in conversations: 
+        conversations[conversation_id] = Conversation() 
+    return conversations[conversation_id] 
+
+
+
+@app.post('/chat/')
+async def chat(input: UserInput) :
+
+    conversation = get_or_create_conversation(input.conversation_id)
+
+    if not conversation.active : 
+        raise HTTPException(status_code=400, detail= "the chat session has ended. please start a new session.")
+    
+    try: 
+        conversation.messages.append({
+            "role" : input.role,
+            "content" : input.message 
+        })
+
+        response = query_groq_api(conversation)
+
+        conversation.messages.append({
+            "role": "assistant",
+            "content" : response
+        })
+
+        return {
+            "response" : response,
+            "conversation_id" : input.conversation_id
+        }
+    except Exception as e :
+        raise HTTPException(status_code=500, detail=str(e))
+    
+if __name__ == "__main__":
+    import uvicorn 
+    uvicorn.run(app, host= "0.0.0.0", port=8000)
